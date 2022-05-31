2914913f3055d11bb7754da24138cd0f11145be8
eshwagadkar <eshwagadkar007@gmail.com>
2022-06-01 01:59:49 +0530

    Build script to compile sass to css file.

---
+++ scss/src/scripts/build.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+const sass = require("node-sass");
+
+const result = sass.renderSync({
+  data: fs.readFileSync(path.resolve("src/global.scss")).toString(),
+  outputStyle: "expanded",
+  outFile: "global.css",
+  includePaths: [path.resolve("/scss/src")],
+});
+
+fs.writeFileSync(path.resolve("src/lib/global.css"), result.css.toString());
