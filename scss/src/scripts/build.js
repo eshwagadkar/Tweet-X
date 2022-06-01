b3c14a69b017b2e432148e6224b773ad03ec1e46
eshwagadkar <eshwagadkar007@gmail.com>
2022-06-01 10:26:23 +0530

    Modify: Build script to compile all scss to css

--- scss/src/scripts/build.js
+++ scss/src/scripts/build.js
@@ -1,12 +1,36 @@
-const fs = require("fs");
-const path = require("path");
-const sass = require("node-sass");
+const Fs = require("fs");
+const Path = require("path");
+const Sass = require("node-sass");
 
-const result = sass.renderSync({
-  data: fs.readFileSync(path.resolve("src/global.scss")).toString(),
-  outputStyle: "expanded",
-  outFile: "global.css",
-  includePaths: [path.resolve("/scss/src")],
+const getComponents = () => {
+  let allComponents = [];
+
+  const types = ["atoms", "molecules", "organisms"];
+
+  types.forEach((type) => {
+    const allFiles = Fs.readdirSync(`src/${type}`).map((file) => ({
+      input: `src/${type}/${file}`,
+      output: `src/lib/${file.slice(0, -4) + "css"}`,
+    }));
+    allComponents = [...allComponents, ...allFiles];
+  });
+
+  return allComponents;
+};
+
+const compile = (path, fileName) => {
+  const result = Sass.renderSync({
+    data: Fs.readFileSync(Path.resolve(path)).toString(),
+    outputStyle: "expanded",
+    outFile: "global.css",
+    includePaths: [Path.resolve("/scss/src")],
+  });
+
+  Fs.writeFileSync(Path.resolve(fileName), result.css.toString());
+};
+
+compile("src/global.scss", "src/lib/global.css");
+
+getComponents().forEach((component) => {
+  compile(component.input, component.output);
 });
-
-fs.writeFileSync(path.resolve("src/lib/global.css"), result.css.toString());
